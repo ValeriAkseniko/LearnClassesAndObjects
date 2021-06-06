3be2aff711c63dc462d22d13b6a3ad85ad5a4b09
ValeriAkseniko <valeri.akseniko@yandex.ru>
2021-06-06 23:31:46 +0300

    addition зукыщтфд агтсешщт

--- LearnClassesAndObjects/Person/PersonFunction.cs
+++ LearnClassesAndObjects/Person/PersonFunction.cs
@@ -12,5 +12,13 @@ namespace LearnClassesAndObjects
         {
             return $"{Name.LastName } {Name.FirstName} {Name.MiddleName} , {Name.Gender} , {Name.Age} years old, height - {Name.Height}cm";
         }
+        static public string GetFullName(Person Name)
+        {
+            return $"{Name.LastName} {Name.FirstName} {Name.MiddleName}";
+        }
+        static public string GetInitial(Person Name)
+        {
+            return $"{Name.LastName} {Name.FirstName[0]}.{Name.MiddleName[0]}.";
+        }
     }
 }
--- LearnClassesAndObjects/Program.cs
+++ LearnClassesAndObjects/Program.cs
@@ -26,19 +26,25 @@ namespace LearnClassesAndObjects
             Person anastasia = new Person("Anastasia", "Malamanova", "Andreevna", "female", 23, 175);
             Person artem = new Person("Artem", "Lipinski", "Dmitrievich", "male", 24, 170);
             Person valera = new Person("Valera", "Akseniko", "Dmitrievich", "male", 24, 178);
-            Person aleksander = new Person("Aleksander", "Egorochkin", "Aleksandrovich", "male", 25, 182);
+            Person aleksandr = new Person("Aleksandr", "Egorochkin", "Aleksandrovich", "male", 25, 182);
             Console.WriteLine(PersonFunction.GetPerson(anastasia));
             Console.WriteLine();
             Console.WriteLine(PersonFunction.GetPerson(artem));
             Console.WriteLine();
             Console.WriteLine(PersonFunction.GetPerson(valera));
             Console.WriteLine();
-            Console.WriteLine(PersonFunction.GetPerson(aleksander));
+            Console.WriteLine(PersonFunction.GetPerson(aleksandr));
+            Console.WriteLine();
+            Console.WriteLine(PersonFunction.GetFullName(anastasia));
+            Console.WriteLine(PersonFunction.GetFullName(aleksandr));
+            Console.WriteLine();
+            Console.WriteLine(PersonFunction.GetInitial(valera));
+            Console.WriteLine(PersonFunction.GetInitial(artem));
             Console.ReadKey();
-            
-            
 
-           
+
+
+
         }
     }
 }
