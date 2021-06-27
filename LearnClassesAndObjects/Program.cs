55b68e75931cf0c66e80a3fd53e2f6362f3f45b5
ValeriAkseniko <valeri.akseniko@yandex.ru>
2021-06-27 21:04:49 +0300

    create sort

--- LearnClassesAndObjects/Program.cs
+++ LearnClassesAndObjects/Program.cs
@@ -12,32 +12,44 @@ namespace LearnClassesAndObjects
         const string fileName = "listPerson.txt";
         static string path = $@"C:\Users\valer\Desktop\{fileName}";
         static PersonService personService = new PersonService();
+        static ProductService productService = new ProductService();
         static void Main(string[] args)
         {
-            Product Sir = new Product
+            Product Cheese = new Product
             {
                 Calories = 140.2,
                 ID = Guid.NewGuid(),
-                Name = "Sir",
+                Name = "Chesse",
                 Weight = 115.3,
                 Price = 250.5m,
                 CreationDate = DateTime.Now
             };
-            Product Sir2 = new Product
+            Product Milk = new Product
             {
                 Calories = 140.2,
                 ID = Guid.NewGuid(),
-                Name = "Sir",
-                Weight = 115.3,
-                Price = 250.5m,
+                Name = "Milk",
+                Weight = 1,
+                Price = 78.5m,
                 CreationDate = DateTime.Now
             };
+            Product Sausage = new Product
+            {
+                Calories = 140.2,
+                ID = Guid.NewGuid(),
+                Name = "Sausage",
+                Weight = 1.5,
+                Price = 215m,
+                CreationDate = DateTime.Now
+            };
+            List<Product> Products = new List<Product>();
+            Products.Add(Cheese);
+            Products.Add(Milk);
+            Products.Add(Sausage);
+            productService.SortByName(Products);
+            productService.Print(Products);
+            
 
-            Sir = Sir2;
-            Sir2.Price = 0.0m;
-
-
-            Console.WriteLine(Sir.GetInfo());
             Console.ReadKey();
         }
 
