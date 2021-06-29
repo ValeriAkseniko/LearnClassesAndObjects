8a6daa5786a10a7d6ce94a7b2db6ca8f63cac893
ValeriAkseniko <valeri.akseniko@yandex.ru>
2021-06-29 21:21:07 +0300

    primer naslidovania

---
+++ LearnClassesAndObjects/TypeProduct/LiquidProduct.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnClassesAndObjects.TypeProduct
+{
+    sealed class LiquidProduct : BaseProduct
+    {
+        const string Unit = "liters";
+        public double Volume { get; set; }
+        public LiquidProduct()
+        {
+
+        }
+        public LiquidProduct(Guid ID, string Name, double Calories, decimal Price, double Volume, DateTime CreationDate)
+            :base(ID,Name,Calories,Price,CreationDate)
+        {
+            this.Volume = Volume;
+        }
+        public override string GetInfo()
+        {            
+            return $"{ID} , {Name} , {Calories} , {Volume} {Unit} , {Price} , {CreationDate}";
+        }
+    }
+}
---
+++ LearnClassesAndObjects/TypeProduct/SolidProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearnClassesAndObjects.TypeProduct
+{
+    sealed class SolidProduct : BaseProduct
+    {
+        const string Unit = "kilograms";
+        public double Weight { get; set; }
+        public override string GetInfo()
+        {
+            return $"{ID} , {Name} , {Calories} , {Weight} {Unit} , {Price} , {CreationDate}";
+        }
+    }
+}
